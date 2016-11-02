bd1e699c46e4186025dc5ae748ccb11288c02083
Kirill Djonua <k.djonua@gmail.com>
2016-11-03 00:17:24 +0300

    exception class InvalidTransportException was created

---
+++ src/exceptions/InvalidTransportException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Kirill Djonua <k.djonua@gmail.com>
+ */
+
+namespace kdjonua\grayii\exceptions;
+
+/**
+ * Class InvalidTransportException
+ * @package kdjonua\grayii\exceptions
+ */
+class InvalidTransportException extends \Exception
+{
+}
