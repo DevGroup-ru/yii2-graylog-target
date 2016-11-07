1026cb1503107aebc04df16a7e0a29b4f59ec528
Kirill Djonua <k.djonua@devgroup.ru>
2016-11-07 16:28:32 +0300

    implements DI container

---
+++ src/transport/HttpTransport.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by Kirill Djonua <k.djonua@gmail.com>
+ */
+
+namespace kdjonua\grayii\transport;
+
+use yii\base\Configurable;
+
+/**
+ * Class HttpTransport
+ * @package kdjonua\grayii\transport
+ */
+class HttpTransport extends \Gelf\Transport\HttpTransport implements Configurable
+{
+
+    public function __construct($config = [])
+    {
+        parent::__construct(
+            $config['host'] ?: null,
+            $config['port'] ?: null,
+            $config['path'] ?: null,
+            $config['sslOptions'] ?: null
+        );
+    }
+}
